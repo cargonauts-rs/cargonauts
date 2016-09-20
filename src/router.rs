efa83cc366f41c5b961a8b1452150365ac397023
Without Boats <woboats@gmail.com>
2016-09-19 21:21:48 -0700

    Dead code.

--- src/router.rs
+++ src/router.rs
@@ -5,8 +5,6 @@ pub trait Router {
     fn attach_index<F: FnOnce(&[String]) -> Self::Response>(&mut self, route: &'static str, f: F);
     fn attach_get<F>(&mut self, route: &'static str, f: F)
         where F: FnOnce(&str, &[String]) -> Self::Response;
-    fn attach_put<F: FnOnce(())>(&mut self, route: &'static str, f: F);
-    fn attach_patch<F: FnOnce(())>(&mut self, route: &'static str, f: F);
 }
 
 pub enum Status {
