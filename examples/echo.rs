7539439d778b2e78755b7e8b080906adc3a531e4
Without Boats <woboats@gmail.com>
2017-05-21 15:55:22 -0700

    Add Echo example.

---
+++ examples/echo.rs
@@ -0,0 +1,33 @@
+#![feature(associated_consts)]
+
+#[macro_use] extern crate cargonauts;
+
+use cargonauts::formats::Debug;
+use cargonauts::methods::Get;
+use cargonauts::resources::{Resource, Environment, Error};
+use cargonauts::futures::{Future, future};
+
+#[derive(Debug)]
+pub struct Echo {
+    echo: String,
+}
+
+impl Resource for Echo {
+    type Identifier = String;
+}
+
+impl Get for Echo {
+    fn get(echo: String, _: &Environment) -> Box<Future<Item = Echo, Error = Error>> {
+        future::ok(Echo { echo }).boxed()
+    }
+}
+
+routes! {
+    resource Echo {
+        method Get in Debug;
+    }
+}
+
+fn main() {
+    cargonauts::server::serve(routes).unwrap();
+}
