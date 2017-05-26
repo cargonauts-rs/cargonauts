7483a39d646230b37f851f97d4e23699f2e60fd6
Without Boats <woboats@gmail.com>
2017-05-26 13:40:10 -0700

    Add missing docs.

---
+++ docs/api/cargonauts/formats/def/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Template",""],["TemplateKey",""]],"trait":[["BuildFormat",""],["Format",""]]});
---
+++ docs/api/implementors/cargonauts/formats/def/trait.BuildFormat.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargonauts"] = ["impl <a class=\"trait\" href=\"cargonauts/formats/def/trait.BuildFormat.html\" title=\"trait cargonauts::formats::def::BuildFormat\">BuildFormat</a> for <a class=\"struct\" href=\"cargonauts/formats/struct.Debug.html\" title=\"struct cargonauts::formats::Debug\">SimpleDebug</a>","impl <a class=\"trait\" href=\"cargonauts/formats/def/trait.BuildFormat.html\" title=\"trait cargonauts::formats::def::BuildFormat\">BuildFormat</a> for <a class=\"struct\" href=\"cargonauts/formats/struct.Handlebars.html\" title=\"struct cargonauts::formats::Handlebars\">Handlebars</a>","impl <a class=\"trait\" href=\"cargonauts/formats/def/trait.BuildFormat.html\" title=\"trait cargonauts::formats::def::BuildFormat\">BuildFormat</a> for <a class=\"struct\" href=\"cargonauts/formats/struct.JsonApi.html\" title=\"struct cargonauts::formats::JsonApi\">JsonApi</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
